a4cebe3aa9ac6ab310ce3dcfe1e0bec2025e1cfb
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-12-16 03:36:33 -0500

    build issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ export default {
     extend: {
       keyframes: {
         fadeInUp: {
-          "0%": { opacity: 0, transform: "translateY(10px)" },
-          "50%": { opacity: 1, transform: "translateY(0)" },
-          "100%": { opacity: 0, transform: "translateY(-10px)" },
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "50%": { opacity: "1", transform: "translateY(0)" },
+          "100%": { opacity: "0", transform: "translateY(-10px)" },
         },
       },
       animation: {
